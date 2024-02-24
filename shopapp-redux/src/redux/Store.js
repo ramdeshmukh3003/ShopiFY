8e5700f473d09e6a93d7a7f4698bd1a886071a4e
Ram Deshmukh <114208322+ramdeshmukh3003@users.noreply.github.com>
2024-02-24 11:54:05 +0530

    Delete shopapp-redux directory

--- shopapp-redux/src/redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { CartSlice } from "./Slices/CartSlice";
-
-export const store = configureStore({
-    reducer:{
-        cart: CartSlice.reducer,
-    }
-});
